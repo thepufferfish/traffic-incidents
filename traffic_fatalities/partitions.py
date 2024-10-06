cb1ff5166ffc6d4ec7cc38fc46809ac2e0274092
thepufferfish <ian.huff2@gmail.com>
2024-10-06 08:31:37 -0700

    feat: add 1 to consoldiation tolerances partitions

--- traffic_fatalities/partitions.py
+++ traffic_fatalities/partitions.py
@@ -1,4 +1,4 @@
 from dagster import DynamicPartitionsDefinition, StaticPartitionsDefinition
 
 nodes_partitions_def = DynamicPartitionsDefinition(name="intersections")
-consolidation_tolerances_partitions_def = StaticPartitionsDefinition(['5', '10', '15', '20', '25', '30'])
+consolidation_tolerances_partitions_def = StaticPartitionsDefinition(['1', '5', '10', '15', '20', '25', '30'])
